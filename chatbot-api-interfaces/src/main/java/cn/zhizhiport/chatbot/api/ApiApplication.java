a86cba8d5466da6a859382681da5b5ce1491b159
ThinkingOverflow <kongjetlin@163.com>
2024-11-21 23:42:21 +0800

    feat: 初始化项目

---
+++ chatbot-api-interfaces/src/main/java/cn/zhizhiport/chatbot/api/ApiApplication.java
@@ -0,0 +1,17 @@
+package cn.zhizhiport.chatbot.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 项目启动类
+ *
+ * @author ThinkingOverflow
+ * @date 2024/11/21
+ */
+@SpringBootApplication
+public class ApiApplication {
+     public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+}
